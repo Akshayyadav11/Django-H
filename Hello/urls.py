c8759b6d36d4c124a13336f467077f609f0470ef
Akshay <akshayy976@gmail.com>
2022-01-05 17:17:46 +0530

    urls added

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+admin.site.site_header = "Ice Cream Admin"
+admin.site.site_title = "Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to Ice Cream Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("", views.index, name="home")
+    path("", views.index, name="home"),
+    path("about", views.about, name="about"),
+    path("services", views.services, name="service"),
+    path("contact", views.contact, name="contact")
 ]
 
